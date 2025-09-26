c352ac5b9a8dcf7e0bc141d327322235b6c75dae
Dawid Kurzyniec <dawid.kurzyniec@gmail.com>
2025-09-26 18:57:28 +0200

    Include also the RGB blinker by default.

--- src/roo_blink.h
+++ src/roo_blink.h
@@ -2,4 +2,9 @@
 
 #include "roo_blink/monochrome/blinker.h"
 #include "roo_blink/monochrome/led.h"
+#include "roo_blink/rgb/blinker.h"
+#include "roo_blink/rgb/led.h"
+
+#ifdef ESP32
 #include "roo_blink/monochrome/led_esp32.h"
+#endif
